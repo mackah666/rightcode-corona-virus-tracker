9298183fd8a5c756e12c120210613f198f9d015e
mackah666 <mackah666@yahoo.co.uk>
2020-06-12 18:57:11 +0100

    Added getters & setters and added List to store data

--- src/main/java/com/rightcode/coronavirustracker/Services/CoronaVirusDataService.java
+++ src/main/java/com/rightcode/coronavirustracker/Services/CoronaVirusDataService.java
@@ -1,6 +1,7 @@
 package com.rightcode.coronavirustracker.Services;
 
 
+import com.rightcode.coronavirustracker.models.LocationStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,12 +14,15 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
--- src/main/java/com/rightcode/coronavirustracker/models/LocationStats.java
+++ src/main/java/com/rightcode/coronavirustracker/models/LocationStats.java
@@ -1,6 +1,30 @@
 package com.rightcode.coronavirustracker.models;
 
 public class LocationStats {
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
     private String state;
     private String country;
     private int latestTotalCases;
