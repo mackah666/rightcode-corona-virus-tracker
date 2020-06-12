3d3d3d518b8f16a267cb656a6790b7fe56ddbefb
mackah666 <mackah666@yahoo.co.uk>
2020-06-12 16:55:22 +0100

    Initial checkin

---
+++ src/main/java/com/rightcode/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rightcode.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rightcode/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rightcode.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
