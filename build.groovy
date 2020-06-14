e52db3f852ab6d6787a3a116f21b1a5e6610a170
mackah666 <mackah666@yahoo.co.uk>
2020-06-14 09:45:56 +0100

    removed maven-plugin rom script

--- build.groovy
+++ build.groovy
@@ -44,9 +44,9 @@ pipeline {
                 }
                 // We never build a SNAPSHOT
                 // We explicitly set versions.
-                sh """
-          mvn -B org.codehaus.mojo:versions-maven-plugin:3.6.3:set -DprocessAllModules -DnewVersion=${VERSION}  $MAVEN_OPTIONS
-      """
+//                sh """
+//          mvn -B org.codehaus.mojo:versions-maven-plugin:3.6.3:set -DprocessAllModules -DnewVersion=${VERSION}  $MAVEN_OPTIONS
+//      """
                 sh """
         mvn -B clean compile $MAVEN_OPTIONS
       """
