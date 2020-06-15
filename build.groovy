b20a18147f4ee6a4ec21b487d9ef34d64f846421
mackah666 <mackah666@yahoo.co.uk>
2020-06-15 10:52:47 +0100

    Added full jar name

--- build.groovy
+++ build.groovy
@@ -66,7 +66,7 @@ pipeline {
                             //sshGet(remote: remote, from: 'abc.sh', into: 'abc.sh', override: true)
                             sshCommand remote: remote, command: "ls -lrt"
                             sshCommand remote: remote, command: "ps -fea|grep -i java"
-                            sshPut remote: remote, from: 'CoronaVirusTrackerCIPipeline/target/*.jar', into: '.'
+                            sshPut remote: remote, from: 'target/coronavirus-tracker-0.0.1-SNAPSHOT.jar', into: '.'
                             //sshCommand remote: remote, command: "pkill -9 -f java"
                             //sshCommand remote: remote, command: "ps -fea|grep -i java"
                             // sshCommand remote: remote, command: "nohup java -jar coronavirus-tracker-0.0.1-SNAPSHOT.jar"
