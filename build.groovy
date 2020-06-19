de457b37f97f754ad589bb5dd02cb0368a83f48d
mackah666 <mackah666@yahoo.co.uk>
2020-06-19 03:28:02 +0100

    Removed all unused stages from buildscripts

--- build.groovy
+++ build.groovy
@@ -1,4 +1,3 @@
-
 // Define variable
 //def remote = [: ]
 
@@ -6,30 +5,13 @@ pipeline {
     agent any
     tools {
         maven 'Maven 3.6.3'
-        //jdk 'jdk8' 
+        //jdk 'jdk8'
     }
     environment {
         MAVEN_OPTIONS = ' -Denv.build-timestamp=${BUILD_TIMESTAMP} ...'
     }
     stages {
-        stage('Initialize') {
-            steps {
-                sh '''
-                    echo "PATH = ${PATH}"
-                    echo "M2_HOME = ${M2_HOME}"
-                '''
-            }
-        }
 
-//        stage('mock Build') {
-//            steps {
-//                echo 'This is a minimal pipeline.'
-//                sh 'env'
-//                sh """
-//                  SHORTREV=`git rev-parse --short HEAD`
-//                  """
-//            }
-//        }
         stage('Build') {
             steps {
                 sh 'mvn -B -DskipTests clean package'
@@ -46,31 +28,25 @@ pipeline {
         stage('Deploy to Server') {
             steps {
                 script {
-//                    stage('nothing but display a message') {
-//                        echo 'First stage'
-//                    }
-                    //stage('ssh') {
+                    def remote = [: ]
+                    remote.name = "k8master"
+                    remote.host = "192.168.1.99"
+                    remote.port = 22
+                    remote.allowAnyHosts = true
+                    withCredentials([usernamePassword(credentialsId: 'pi-server', passwordVariable: 'password', usernameVariable: 'userName')]) {
 
-                        def remote = [: ]
-                        remote.name = "k8master"
-                        remote.host = "192.168.1.99"
-                        remote.port = 22
-                        remote.allowAnyHosts = true
-                        withCredentials([usernamePassword(credentialsId: 'pi-server',passwordVariable: 'password', usernameVariable: 'userName')]) {
+                        remote.user = userName
+                        remote.password = password
 
-                            remote.user = userName
-                            remote.password = password
+                        //sshGet(remote: remote, from: 'abc.sh', into: 'abc.sh', override: true)
+                        sshCommand remote: remote, command: "ls -lrt"
+                        sshCommand remote: remote, command: "ps -fea|grep -i java"
+                        sshCommand remote: remote, command: "pkill -f java"
+                        sshPut remote: remote, from: 'target/coronavirus-tracker-0.0.1-SNAPSHOT.jar', into: '.'
+                        sshCommand remote: remote, command: "screen -d -m java -jar coronavirus-tracker-0.0.1-SNAPSHOT.jar"
+                        //sshCommand remote: remote, command: "~."
 
-                            //sshGet(remote: remote, from: 'abc.sh', into: 'abc.sh', override: true)
-                            sshCommand remote: remote, command: "ls -lrt"
-                            sshCommand remote: remote, command: "ps -fea|grep -i java"
-                            sshCommand remote: remote, command: "pkill -f java"
-                            sshPut remote: remote, from: 'target/coronavirus-tracker-0.0.1-SNAPSHOT.jar', into: '.'
-                            sshCommand remote: remote, command: "screen -d -m java -jar coronavirus-tracker-0.0.1-SNAPSHOT.jar"
-                            //sshCommand remote: remote, command: "~."
-
-                        }
-                    //}
+                    }
                 }
             }
         }
